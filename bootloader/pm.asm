1e7a06ba2ec02b7e95591c6afb6e92e027ae941f
Tooghi <tooghip@gmail.com>
2022-03-19 13:51:04 +0330

    Enable the A20 line in protected mode

--- bootloader/pm.asm
+++ bootloader/pm.asm
@@ -9,6 +9,12 @@ PMEntry:
       mov byte[0xb8000], 'p'
       mov byte[0xb8001], 0xa
 
+      ; enable the A20 line
+      ; https://wiki.osdev.org/A20_Line
+      in al, 0x92
+      or al, 2
+      out 0x92, al
+
       jmp PBEnd
 
 PBEnd:
